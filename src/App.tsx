a88d1850dcbc6fed3ff65dbd453fd18ddb4a421b
shubhampatel70 <shubhampatel26515@gmail.com>
2024-07-10 23:23:28 +0530

    change some ui for whole project and for header too

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-
 // import "./App.css";
 
 // import { Home } from "@mui/icons-material";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path='/' element={<Navbar/>} >
-            <Route path='' element={<Home/>} ></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navbar />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
     </>
-    );
+  );
 }
 
 export default App;

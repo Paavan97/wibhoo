a3becdd4b5738c97a58ff705f4e5c0df998fd02d
bhaviksinh <bhavik@tuvoc.com>
2024-07-10 17:39:37 +0530

    first commit stagging

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,12 @@
-import { Button } from '@mui/material'
-import React from 'react'
+import { Button } from "@mui/material";
+import React from "react";
 
-const Home:React.FC = () => {
+const Home: React.FC = () => {
   return (
     <div>
-        <Button variant="contained">Text</Button>
+      <Button variant="contained">Click me</Button>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
